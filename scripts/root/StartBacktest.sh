3a36700379d6aef17205ae626105dd5882da29bb
xyhuang <huangxy17@fudan.edu.cn>
2019-12-18 20:16:17 +0800

    revise script

--- scripts/root/StartBacktest.sh
+++ scripts/root/StartBacktest.sh
@@ -2,7 +2,9 @@
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
-cd /today
-./bin/backtest > backtest.log 2>&1 &!
+cd ~/hft
+make
+cd ~/hft/build/bin
+./backtest > backtest.log 2>&1 &!
 mv order.dat order_backtest.dat
 cp order_backtest.dat mid_backtest.dat /today
