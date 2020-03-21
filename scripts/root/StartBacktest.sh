46b2b5b81d7e42c0f9926c9d8a7133345ce75dbc
xyhuang <huangxy17@fudan.edu.cn>
2020-03-21 17:22:07 +0800

    upload

--- scripts/root/StartBacktest.sh
+++ scripts/root/StartBacktest.sh
@@ -2,12 +2,14 @@
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
+date=`date  +"%Y-%m-%d"`
+
 cd ~/hft
 make
 cd ~/hft/build/bin
 ./backtest > backtest.log 2>&1
 
 sleep 5
-mv order.dat order_backtest.dat
-mv mid.dat mid_backtest.dat
+mv backtest_out/order_$date.dat order_backtest.dat
+mv backtest_out/mid_$date.dat mid_backtest.dat
 cp order_backtest.dat mid_backtest.dat /today
