bcefa25d4fa14ad50d2d507d6e5d53acf6c2ab84
nick <nick@localhost.localdomain>
2020-04-27 10:10:17 +0800

    upload

--- src/ctporder/main.cpp
+++ src/ctporder/main.cpp
@@ -65,9 +65,9 @@ int main() {
   }
   CThostFtdcTraderApi* user_api = CThostFtdcTraderApi::CreateFtdcTraderApi();
 
-  std::string broker = "9999";
-  std::string username = "116909";
-  std::string password = "yifeng";
+  std::string broker = "0034";
+  std::string username = "63056012";
+  std::string password = "wmby2020";
   /*
   std::string broker = "9999";
   std::string username = "115686";
@@ -108,7 +108,8 @@ int main() {
 
   user_api->SubscribePrivateTopic(THOST_TERT_QUICK);
   user_api->SubscribePublicTopic(THOST_TERT_QUICK);
-  std::string counterparty_host = "tcp://180.168.146.187:10100";
+  // std::string counterparty_host = "tcp://180.168.146.187:10100";
+  std::string counterparty_host = "tcp://180.166.0.229:21405";
   user_api->RegisterFront(const_cast<char*>(counterparty_host.c_str()));
   user_api->Init();
   if (enable_file) {
--- src/manual_ctp/main.cpp
+++ src/manual_ctp/main.cpp
@@ -3,6 +3,7 @@
 #include <struct/order.h>
 #include <struct/exchange_info.h>
 #include <struct/order_side.h>
+#include <struct/order_action.h>
 
 #include <util/sender.hpp>
 #include <util/recver.hpp>
@@ -15,42 +16,63 @@ void RunSend(Sender<Order> * sender) {
   int count = 0;
   std::string ticker;
   std::string buffer;
-  while (ticker != "quit") {
-    std::cout << "ticker:";
-    std::getline(std::cin, buffer);
-    ticker = buffer;
-    std::cout << "price:";
-    std::getline(std::cin, buffer);
-    double price = atof(buffer.c_str());
-    std::cout << "size:";
-    std::getline(std::cin, buffer);
-    int size = atoi(buffer.c_str());
-    std::cout << "side:(1-buy, 2-sell)";
-    std::getline(std::cin, buffer);
-    int side_int = atoi(buffer.c_str());
-    if (side_int != 1 and side_int != 2) {
-      cout << "wrong side, 1->buy 2->sell!" << endl;
-      continue;
-    }
-    OrderSide::Enum side = (side_int == 1) ? OrderSide::Buy : OrderSide::Sell;
-    char buf[1024];
-    snprintf(buf, sizeof(buf), "Confirm OrderInfo:%s %lf@%d; 1-confirmed other-quit:", ticker.c_str(), price, size);
-    std::cout << buf;
-    std::getline(std::cin, buffer);
-    int confirmed = atoi(buffer.c_str());
-    if (confirmed == 1) {
+  std::string action;
+  while (action != "quit") {
+    std::cout << "action:";
+    std::getline(std::cin, action);
+    if (action == "new") {
+      std::cout << "ticker:";
+      std::getline(std::cin, buffer);
+      ticker = buffer;
+      std::cout << "price:";
+      std::getline(std::cin, buffer);
+      double price = atof(buffer.c_str());
+      std::cout << "size:";
+      std::getline(std::cin, buffer);
+      int size = atoi(buffer.c_str());
+      std::cout << "side:(1-buy, 2-sell)";
+      std::getline(std::cin, buffer);
+      int side_int = atoi(buffer.c_str());
+      if (side_int != 1 and side_int != 2) {
+        cout << "wrong side, 1->buy 2->sell!" << endl;
+        continue;
+      }
+      OrderSide::Enum side = (side_int == 1) ? OrderSide::Buy : OrderSide::Sell;
+      char buf[1024];
+      snprintf(buf, sizeof(buf), "Confirm OrderInfo:%s %lf@%d; 1-confirmed other-quit:", ticker.c_str(), price, size);
+      std::cout << buf;
+      std::getline(std::cin, buffer);
+      int confirmed = atoi(buffer.c_str());
+      if (confirmed == 1) {
+        Order* o = new Order;
+        o->action = OrderAction::NewOrder;
+        snprintf(o->ticker, sizeof(o->ticker), "%s", ticker.c_str());
+        o->price = price;
+        o->side = side;
+        o->size = size;
+        snprintf(o->order_ref, sizeof(o->order_ref), "%s%d", ticker.c_str(), count++);
+        sender->Send(*o);
+        o->Show(stdout);
+        std::cout << "*****************" <<  o->order_ref << " sent*****************\n";
+        sleep(2);
+      } else {
+        std::cout << "*****************Order Quit!*****************" << std::endl;
+        continue;
+      }
+    } else if (action == "cancel") {
+      std::cout << "order_ref:";
+      std::string order_ref;
+      std::getline(std::cin, order_ref);
       Order* o = new Order;
+      o->action = OrderAction::CancelOrder;
+      snprintf(o->order_ref, sizeof(o->order_ref), "%s", order_ref.c_str());
       snprintf(o->ticker, sizeof(o->ticker), "%s", ticker.c_str());
-      o->price = price;
-      o->side = side;
-      o->size = size;
-      snprintf(o->order_ref, sizeof(o->order_ref), "%s%d", ticker.c_str(), count++);
       sender->Send(*o);
-      std::cout << "*****************" <<  o->order_ref << " sent*****************\n";
-      sleep(3);
+      o->Show(stdout);
+      std::cout << "*****************" <<  o->order_ref << "Cancel Sent*****************\n";
+      sleep(2);
     } else {
-      std::cout << "*****************Order Quit!*****************" << std::endl;
-      continue;
+      std::cout << "invaild action\n";
     }
   }
   exit(1);
